6a647fb3b244644eace3c3cbbe2878c8e5e84504
Mel Barbosa Tamboril <meltamboril@gmail.com>
2025-09-23 02:47:18 -0300

    PIES-5: Teste de integracao

--- src/pies/code/fonte/Main.java
+++ src/pies/code/fonte/Main.java
@@ -2,6 +2,6 @@ package pies.code.fonte;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("OLÁ EQUIPE 4! AMO VOCÊS!AAAAAA!");
+        System.out.println("teste de projeto!");
     }
 }
