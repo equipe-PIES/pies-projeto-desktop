d505372814f7695e39f6e682e3ac0df9c0a33a6a
Mel Barbosa Tamboril <meltamboril@gmail.com>
2025-09-23 02:55:12 -0300

    PIES-5 Atualização de Teste

--- src/pies/code/fonte/Main.java
+++ src/pies/code/fonte/Main.java
@@ -2,6 +2,6 @@ package pies.code.fonte;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("teste de projeto!");
+        System.out.println("NANA COMOM COMOM COMO");
     }
 }
