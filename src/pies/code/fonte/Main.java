34c6bb58058a27fe28cf49882188bf50acc181e5
Mel Barbosa Tamboril <meltamboril@gmail.com>
2025-09-23 02:26:49 -0300

    PIES-5: Teste de branch OK

--- src/pies/code/fonte/Main.java
+++ src/pies/code/fonte/Main.java
@@ -2,6 +2,6 @@ package pies.code.fonte;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("OLÁ EQUIPE 4! AMO VOCÊS!!");
     }
 }
