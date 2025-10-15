8b4f993a651a8bf719c2929328f151088122b6e7
anaradlei <anaradlei5@gmail.com>
2025-10-14 21:41:46 -0300

    WIP

--- Pies-Backend/src/main/java/com/pies/api/projeto/integrado/Pies_Backend/controller/AuthenticationController.java
+++ Pies-Backend/src/main/java/com/pies/api/projeto/integrado/Pies_Backend/controller/AuthenticationController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController //indicando que é um controller rest
 @RequestMapping("auth")//mapeando o endpoint na qual esse controller vai ser chamado, se chamar auth vai cair nesse endpoint
+
 public class AuthenticationController {//endpoint pro usuario fazer login
     @Autowired
     private AuthenticationManager authenticationManager; //
@@ -17,8 +18,18 @@ public class AuthenticationController {//endpoint pro usuario fazer login
 
     @PostMapping("/login")
     public ResponseEntity login(@RequestBody @Valid AuthenticationDTO data){
+
         var usernamepassword = new UsernamePasswordAuthenticationToken(data.login(),data.password());
         var auth = this.authenticationManager.authenticate(usernamepassword);//recebe o login e a senha juntos formados como um token
+
         return ResponseEntity.ok().build();
+
     }
+    //novos usuarios
+
+    @PostMapping("/register")
+    public ResponseEntity register(@RequestBody @Valid RegisterDTO data){
+
+    }
+
 }
