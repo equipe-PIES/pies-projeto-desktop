9ff732ac7a3fbe7ee5518a65a4dfa49cca00ad35
Layssa Oliveira <layssaoliveira626@gmail.com>
2025-10-25 12:09:48 -0300

    reiniciando projeto de uniao

--- Pies-front/src/main/java/com/pies/projeto/integrado/piesfront/Launcher.java
+++
@@ -1,9 +0,0 @@
-package com.pies.projeto.integrado.piesfront;
-
-import javafx.application.Application;
-
-public class Launcher {
-    public static void main(String[] args) {
-        Application.launch(HelloApplication.class, args);
-    }
-}
