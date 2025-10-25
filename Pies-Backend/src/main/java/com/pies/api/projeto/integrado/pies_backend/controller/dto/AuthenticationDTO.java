00d917c4df66b5fab9e2d1c6039973a10ce71f2e
Layssa Oliveira <layssaoliveira626@gmail.com>
2025-10-25 09:20:22 -0300

    remoção das pastas e adicionando gitignore

--- Pies-Backend/src/main/java/com/pies/api/projeto/integrado/pies_backend/controller/dto/AuthenticationDTO.java
+++
@@ -1,4 +0,0 @@
-package com.pies.api.projeto.integrado.pies_backend.controller.dto;
-
-public record AuthenticationDTO(String login, String password) {
-}
--- Pies-Backend/src/main/java/com/pies/api/projeto/integrado/pies_backend/controller/dto/LoginResponseDTO.java
+++
@@ -1,4 +0,0 @@
-package com.pies.api.projeto.integrado.pies_backend.controller.dto;
-
-public record LoginResponseDTO(String accessToken) {
-}
