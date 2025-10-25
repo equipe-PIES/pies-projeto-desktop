315a1964c5e4c3cb17257a4f8c108313d13b670b
Layssa Oliveira <layssaoliveira626@gmail.com>
2025-10-25 12:05:05 -0300

    fazendo ajustes dos problemas de front e dependencias

--- Pies-front/src/main/java/com/PiesFrontApplication.java
+++ Pies-front/src/main/java/com/PiesFrontApplication.java
@@ -13,10 +13,15 @@ public class PiesFrontApplication extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(PiesFrontApplication.class.getResource("/com/pies/projeto/integrado/piesfront/screens/tela-de-login.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(PiesFrontApplication.class
+                .getResource("/com/pies/projeto/integrado/piesfront/screens/tela-de-login.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1200, 800);
         stage.setTitle("Amparo Edu");
         stage.setScene(scene);
         stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
--- Pies-front/src/main/java/module-info.java
+++ Pies-front/src/main/java/module-info.java
@@ -1,10 +1,17 @@
 module com.pies.projeto.integrado.piesfront {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.net.http;
 
     requires org.controlsfx.controls;
     requires org.kordamp.bootstrapfx.core;
 
-    opens com.pies.projeto.integrado.piesfront to javafx.fxml;
-    exports com.pies.projeto.integrado.piesfront;
+    opens com.pies.projeto.integrado.piesfront.controllers to javafx.fxml;
+    opens com.pies.projeto.integrado.piesfront.dto;
+    opens com to javafx.fxml;
+
+    exports com;
+    exports com.pies.projeto.integrado.piesfront.controllers;
+    exports com.pies.projeto.integrado.piesfront.services;
+    exports com.pies.projeto.integrado.piesfront.dto;
 }
