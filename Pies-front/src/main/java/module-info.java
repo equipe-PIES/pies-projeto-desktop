28f611f2a41ee7f59c2a6d921e17f972fcb1b5e2
Mel Barbosa Tamboril <meltamboril@gmail.com>
2025-10-30 18:40:12 -0300

    PIES-41: Editei os containers das screens

--- Pies-front/src/main/java/module-info.java
+++ Pies-front/src/main/java/module-info.java
@@ -1,14 +1,18 @@
 module com.pies.projeto.integrado.piesfront {
     requires javafx.controls;
     requires javafx.fxml;
-    requires javafx.graphics; // Stage, Scene, Color, etc.
-    requires javafx.base;     // ObservableList, etc.
-    requires javafx.web;      // WebView/WebEngine
-    requires javafx.media;    // Se for usar mídia
+    requires java.net.http;
 
     requires org.controlsfx.controls;
     requires org.kordamp.bootstrapfx.core;
+    requires com.fasterxml.jackson.databind;
 
-    opens com.pies.projeto.integrado.piesfront to javafx.fxml, javafx.graphics;
-    exports com.pies.projeto.integrado.piesfront;
+    opens com.pies.projeto.integrado.piesfront.controllers to javafx.fxml;
+    opens com.pies.projeto.integrado.piesfront.dto;
+    opens com to javafx.fxml;
+
+    exports com;
+    exports com.pies.projeto.integrado.piesfront.controllers;
+    exports com.pies.projeto.integrado.piesfront.services;
+    exports com.pies.projeto.integrado.piesfront.dto;
 }
