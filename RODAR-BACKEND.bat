72e4f9d032957f0bdadcea981c7c33e3f35b511e
Layssa Oliveira <layssaoliveira626@gmail.com>
2025-11-03 10:08:48 -0300

    ajustando conflitos entre front, back e banco e deixado login funcional

--- RODAR-BACKEND.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cd /d "%~dp0\Pies-Backend"
-echo ========================================
-echo INICIANDO BACKEND
-echo ========================================
-echo.
-mvn spring-boot:run
-pause
--- remove-constraint.sql
+++
@@ -1,6 +0,0 @@
--- Execute este comando no SQL Editor do Supabase
--- Isso vai remover a constraint que está bloqueando o registro
-
-ALTER TABLE users DROP CONSTRAINT IF EXISTS users_role_check;
-
--- Pronto! Agora você pode registrar usuários
