5e8f84c7a685b0c757c003b2abe6d812106fbb6c
gabrielrbsousa <gabrielribeiro@alu.ufc.br>
2025-11-25 21:23:03 -0300

    Atualizaçãodos FXMLs, estilos e ícones

--- Pies-front/src/main/java/com/PiesFrontApplication.java
+++ Pies-front/src/main/java/com/PiesFrontApplication.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.text.Font;
 
 import java.io.IOException;
 
@@ -13,7 +14,9 @@ public class PiesFrontApplication extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(PiesFrontApplication.class.getResource("/com/pies/projeto/integrado/piesfront/screens/tela-de-login.fxml"));
+        stage.setMinWidth(1000);
+        stage.setMinHeight(600);
+        FXMLLoader fxmlLoader = new FXMLLoader(PiesFrontApplication.class.getResource("/com/pies/projeto/integrado/piesfront/screens/cadastro-de-aluno.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1200, 800);
         stage.setTitle("Amparo Edu");
         stage.setScene(scene);
